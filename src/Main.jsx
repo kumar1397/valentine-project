51387dc7c84761c7cb20aad486282fff6f84c3bd
kumar1397 <sahuiswar007@outlook.com>
2024-06-30 18:19:04 +0530

    2

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import data from './content.json'
 import { useNavigate } from "react-router-dom";
 export default function  Main (){
-    const [height, setHeight] = useState(24); // Initial height in Tailwind units
-    const [width, setWidth] = useState(36); // Initial height in Tailwind units
-    const [text, setText] = useState(24);
+    const [height, setHeight] = useState(28); // Initial height in Tailwind units
+    const [width, setWidth] = useState(40); // Initial height in Tailwind units
+    const [text, setText] = useState(28);
     const [title, setTitle] = useState(0);
     const handleClick = () => {
         if (title > 13){
@@ -14,9 +14,9 @@ export default function  Main (){
             setTitle(title + 1);
         }
         console.log(title);
-        setHeight(height + 3);
-        setWidth(width + 3)
-        setText(text + 3);
+        setHeight(height + 5);
+        setWidth(width + 5)
+        setText(text + 5);
     };
     const Navigate = useNavigate();
     const handleButton = () =>{
@@ -30,9 +30,9 @@ export default function  Main (){
                 <div className='text-3xl font-medium text-black '>Will you be my valentine??</div>
 
             </div>
-            <div className="w-full h-[50vh] flex flex-row gap-6 justify-around items-center text-2xl font-semibold">
-                <button onClick={handleButton}  className={`bg-green-400 text-white }`} style={{ height: `${height * 4}px`, width: `${width * 4}px`, fontSize: `${text}px` }}>YES</button>
-                <button onClick={handleClick} className={`bg-red-400 text-white h-24 w-36`}>{data[title].name}</button>
+            <div className="w-full h-[50vh] flex flex-row gap-6 items-center text-3xl font-semibold">
+                <button onClick={handleButton}  className={`bg-green-400 text-white fixed left-56 bottom-32 rounded-xl z-10`} style={{ height: `${height * 4}px`, width: `${width * 4}px`, fontSize: `${text}px` }}>YES</button>
+                <button onClick={handleClick} className={`bg-red-400 text-white h-28 w-40 fixed right-56 bottom-32 rounded-xl`}>{data[title].name}</button>
             </div>
         </div>
     );
